dc2bc7ba4d49837951ea898ebf9ff28d36e7a996
lemon857 <lemon857.an@gmail.com>
2026-07-01 18:24:42 +0500

    fix: starting nginx after configure config

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
-#! /bin/bash
+#!/bin/sh
+set -eu
 
-echo $(cat /nginx-base.conf) | envsubst > /etc/nginx/conf.d/default.conf
+if [ ! -f /etc/nginx/conf.d/default.conf ]; then
+	if [ -z "${DOMAIN_NAME:-}" ]; then
+		echo "ERROR: DOMAIN_NAME is not set" >&2
+		exit 1
+	fi
+
+  echo "$(cat /nginx-base.conf)" | envsubst > /etc/nginx/conf.d/default.conf
+fi
+
+exec nginx -g 'daemon off;'
